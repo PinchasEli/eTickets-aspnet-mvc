d5b034f985b7254beff56d0fdc647a76811780a7
Pinchas Elia Nachmani <pinchas28@gmail.com>
2024-01-28 12:56:07 +0200

    start

---
+++ eTickets/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    // Add DbSet properties for your models
+    // public DbSet<YourModel> YourModels { get; set; }
+}
